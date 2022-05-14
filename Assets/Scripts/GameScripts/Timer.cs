66473e95641b13c4d7b9b05d8be41b29ac3a6f22
Zwangsjacke <102676338+Zwangsjacke@users.noreply.github.com>
2022-05-14 23:47:54 +0200

    Build Timer
    Still ugly

---
+++ Assets/Scripts/GameScripts/Timer.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public bool timerActive = false;
+    float currentTime;
+    public int startingMinutes;
+    public TextMeshProUGUI currentTimeText;
+    private void Awake()
+    {
+        currentTime = startingMinutes * 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timerActive)
+        {
+            currentTime -= Time.deltaTime;
+            if(currentTime <= 0)
+            {
+                TimerFinished();
+            }
+        }
+
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.Minutes.ToString() + ":" + time.Seconds.ToString();
+    }
+
+
+    public void StartTimer()
+    {
+        timerActive = true;
+    }
+
+    public void EndTimer()
+    {
+        timerActive = false;
+    }
+
+    public void TimerFinished()
+    {
+        timerActive = false;
+        Debug.Log("Timer finished");
+    }
+}
